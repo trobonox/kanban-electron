1af113dddaf3f6dce650fb2d376dc8359fc9e9c6
trobonox <57040351+trobonox@users.noreply.github.com>
2022-04-18 19:42:03 +0200

    Add correct light mode hex values to themes.js

--- src/renderer/themes.js
+++ src/renderer/themes.js
@@ -1,16 +1,16 @@
 export const light = {
-  bgPrimary: "#1E1E2E",
-    elevation1: "#302D41",
-    elevation2: "#575268",
-    elevation3: "#6E6C7E",
-    accent: "#F28FAD",
-    accentDarker: "#c97790",
-    text: "#f4f4f5",
-    textD1: "#e4e4e7",
-    textD2: "#d4d4d8",
-    textD3: "#a1a1aa",
-    textD4: "#71717a",
-    textButtons: "#1A1826",
+  bgPrimary: "#f1f5f9",
+  elevation1: "#e2e8f0",
+  elevation2: "#cbd5e1",
+  elevation3: "#94a3b8",
+  accent: "#1d4ed8",
+  accentDarker: "#1e40af",
+  text: "#1e293b",
+  textD1: "#334155",
+  textD2: "#475569",
+  textD3: "#64748b",
+  textD4: "#94a3b8",
+  textButtons: "#f1f5f9",
 }
 
 export const dark = {
@@ -25,6 +25,7 @@ export const dark = {
   textD2: "#d4d4d8",
   textD3: "#a1a1aa",
   textD4: "#71717a",
+  textButtons: "#f1f5f9",
 }
 
 export const catppuccin = {
