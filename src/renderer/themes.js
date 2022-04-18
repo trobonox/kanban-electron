af2415114e08e90c39d4cf38770ac20c6611b2af
trobonox <57040351+trobonox@users.noreply.github.com>
2022-04-18 19:13:59 +0200

    Add theme switching for curated themes

---
+++ src/renderer/themes.js
@@ -0,0 +1,49 @@
+export const light = {
+  bgPrimary: "#1E1E2E",
+    elevation1: "#302D41",
+    elevation2: "#575268",
+    elevation3: "#6E6C7E",
+    accent: "#F28FAD",
+    accentDarker: "#c97790",
+    text: "#f4f4f5",
+    textD1: "#e4e4e7",
+    textD2: "#d4d4d8",
+    textD3: "#a1a1aa",
+    textD4: "#71717a",
+    textButtons: "#1A1826",
+}
+
+export const dark = {
+  bgPrimary: "#070c10",
+  elevation1: "#181a20",
+  elevation2: "#272b34",
+  elevation3: "#4b5364",
+  accent: "#2563eb",
+  accentDarker: "#1d4ed8",
+  text: "#f4f4f5",
+  textD1: "#e4e4e7",
+  textD2: "#d4d4d8",
+  textD3: "#a1a1aa",
+  textD4: "#71717a",
+}
+
+export const catppuccin = {
+  bgPrimary: "#1E1E2E",
+  elevation1: "#302D41",
+  elevation2: "#575268",
+  elevation3: "#6E6C7E",
+  accent: "#F28FAD",
+  accentDarker: "#c97790",
+  text: "#f4f4f5",
+  textD1: "#e4e4e7",
+  textD2: "#d4d4d8",
+  textD3: "#a1a1aa",
+  textD4: "#71717a",
+  textButtons: "#1A1826",
+}
+
+export default {
+  light,
+  dark,
+  catppuccin
+}
