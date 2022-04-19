d41238197e2c652f57798e2941fe6a6b25f96c3c
trobonox <57040351+trobonox@users.noreply.github.com>
2022-04-19 13:43:01 +0200

    Polish home page styling

--- src/renderer/pages/index.vue
+++ src/renderer/pages/index.vue
@@ -1,19 +1,18 @@
 <template>
   <div class="px-6">
     <Navbar />
-    <main id="home">
+    <main id="home" class="mx-4">
       <section
         id="board-selection"
         class="mt-4 mb-10 ml-2 flex flex-col items-start"
       >
-        <h4 class="mb-8 text-4xl font-bold">Welcome back!</h4>
-        <h4 class="mb-2 text-xl font-medium">Your boards:</h4>
-        <button
-          class="text-buttons bg-accent mb-6 rounded-md px-2 py-1"
-          @click="createNewBoard"
+        <h1 class="text-4xl font-bold">Welcome back!</h1>
+        <span class="text-dim-3 mb-8"
+          >Your {{ this.boards.length }} boards are ready and waiting for
+          you.</span
         >
-          Create new board
-        </button>
+        <h2 class="mb-2 text-2xl font-bold">Your boards:</h2>
+
         <span v-if="!boards || boards.length == 0"
           >Wowzers! Looks like you have no boards yet! Use the button above to
           create a new one.</span
@@ -26,6 +25,26 @@
             class="bg-elevation-1 bg-elevation-2-hover rounded-md p-4 text-lg"
             >{{ board.title }}</nuxt-link
           >
+          <button
+            class="bg-elevation-1 bg-elevation-2-hover rounded-md px-4 py-2 text-lg"
+            @click="createNewBoard"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="text-accent h-8 w-8"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+              />
+            </svg>
+            <span hidden>Add Board</span>
+          </button>
         </div>
       </section>
     </main>
