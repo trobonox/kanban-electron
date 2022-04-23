b11945b282864e2466bebe6a20d728a923beca63
trobonox <57040351+trobonox@users.noreply.github.com>
2022-04-23 19:57:06 +0200

    Make external links open in external browser

--- src/main/mainWindow.js
+++ src/main/mainWindow.js
@@ -1,15 +1,19 @@
-import BrowserWinHandler from './BrowserWinHandler'
+import BrowserWinHandler from "./BrowserWinHandler";
 
 const winHandler = new BrowserWinHandler({
   height: 600,
   width: 1000,
-  autoHideMenuBar: true
-})
+  autoHideMenuBar: true,
+});
 
-winHandler.onCreated(_browserWindow => {
-  winHandler.loadPage('/')
+winHandler.onCreated((_browserWindow) => {
+  winHandler.loadPage("/");
   // Or load custom url
   // _browserWindow.loadURL('https://google.com')
-})
+  _browserWindow.webContents.on("new-window", function (e, url) {
+    e.preventDefault();
+    require("electron").shell.openExternal(url);
+  });
+});
 
-export default winHandler
+export default winHandler;
