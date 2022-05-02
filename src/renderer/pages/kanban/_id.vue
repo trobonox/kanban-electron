821c3bc9f0d27d0e966064d1f4f8e87958482452
trobonox <57040351+trobonox@users.noreply.github.com>
2022-05-02 20:11:11 +0200

    Start UI revamp by making top bar buttons more subtle

--- src/renderer/pages/kanban/_id.vue
+++ src/renderer/pages/kanban/_id.vue
@@ -24,15 +24,45 @@
     />
     <div class="flex flex-row gap-4">
       <nuxt-link to="/"
-        ><button class="text-buttons bg-accent mb-6 rounded-md p-2">
-          Go back home
+        ><button
+          class="bg-elevation-1 bg-elevation-2-hover mb-6 flex flex-row items-center justify-between gap-2 rounded-md px-2 py-1"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="text-accent h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z"
+            />
+          </svg>
+          <span>Go back home</span>
         </button>
       </nuxt-link>
       <button
-        class="text-buttons bg-accent mb-6 rounded-md p-2"
+        class="bg-elevation-1 bg-elevation-2-hover mb-6 flex flex-row items-center justify-between gap-2 rounded-md px-2 py-1"
         @click="deleteBoard"
       >
-        Delete Board
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="text-accent h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+          />
+        </svg>
+        <span>Delete Board</span>
       </button>
     </div>
     <div
